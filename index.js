81573d67f242c8e0a7cf4269a3536b9ed7933124
lixinliang <393464140@qq.com>
2019-03-06 16:33:56 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,4 +2,19 @@
 
 const hapi = require('hapi')
 
-console.log(hapi)
+const server = hapi.server({ port: 8003 })
+
+server.route({
+  method: 'get',
+  path: '/',
+  handler (req) {
+    console.log('')
+    console.log(req['path'])
+    console.log(req['headers']['user-agent'])
+    return 'hello world'
+  }
+})
+
+server.start()
+
+console.log('server is running on "http://localhost:8003/"')
